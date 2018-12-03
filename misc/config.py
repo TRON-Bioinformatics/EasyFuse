57e2da8683808a90af14bf23987ebc0046f06300
Patrick Sorn <patrick.sorn@tron-mainz.de>
2018-12-03 15:25:59 +0100

    Merged some misc scripts and deleted redundant code

--- misc/config.py
+++ misc/config.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
 Reading/accessing "config.ini" file
 
@@ -8,19 +6,15 @@ Reading/accessing "config.ini" file
 """
 
 
-import sys
-
 class Config(object):
-    """ Class to hold all state information about a run, 
-    can be used to restart after a break point 
-    """
-    def __init__(self,infile):
-        """ inits the object and reads a file with state info """
+    """Class to access all required parameter information about a run"""
+    def __init__(self, infile):
+        """inits the object and reads a file with state info"""
         self._file = infile
         self._categories, self._d, self._key_lists = self._read_config_file()
     
     def _read_config_file(self):
-        """ read key-value pairs from the state file self._file """
+        """read key-value pairs from the state file self._file"""
         record_dict = {}
         key_lists = {}
         categories = []
@@ -39,22 +33,28 @@ class Config(object):
                         words = line.split("=")
                         words = [x.strip() for x in words]
                         if len(words) < 2:
-                            continue;
+                            continue
                         temp_d = record_dict.get(category, {})
                         temp_d[words[0]] = words[1]
                         record_dict[category] = temp_d
                         temp_l = key_lists.get(category, [])
                         temp_l.append(words[0])
                         key_lists[category] = temp_l
-        return categories, d, key_lists
+        return categories, record_dict, key_lists
  
   
-    def get(self, category,key):
+    def get(self, category, key):
         """get an dictionary entry named "key" from self._d"""
         return self._d[category][key]
+
+    def get_path(self):
+        """return the fullpath string of this config file"""
+        return self._file
   
     def get_categories(self):
+        """return a list of catgories"""
         return self._categories
 
-    def get_keys(self,category):
+    def get_keys(self, category):
+        """return a list of keys belonging to a defined category"""
         return self._key_lists[category]
