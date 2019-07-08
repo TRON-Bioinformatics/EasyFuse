98a3cf3b9bbf05d6073864b5ebce3ee69b3ce214
urs.lahrmann <urs.lahrmann@biontech.de>
2019-07-08 12:55:48 +0200

    Feature update: Changed process monitoring from flat file to sqlite3 db

--- misc/samples.py
+++ misc/samples.py
@@ -8,12 +8,12 @@ in a dict with sample identifiers as keys and data as value. Data itself is
 are 2-tuples of lists, currently including an identifier of the tools that have been
 run on the sample and the fastq files corresponding to the sample.
 
-@author: Tron (PASO), BNT (URLA)
-@version: 20181126
+@author: BNT (URLA)
+@version: 20190507
 """
 
 from __future__ import print_function
-import os
+import sqlite3
 from argparse import ArgumentParser
 
 # pylint: disable=line-too-long
@@ -23,42 +23,29 @@ class Samples(object):
     def __init__(self, infile):
         """Parameter initialization"""
         self.infile = infile
-        if not os.path.exists(self.infile):
-            open(self.infile, "a").close()
-        self.sample_map = self.read_file()
-
-    # read and write sample file
-    def read_file(self):
-        """Read a sample file and return a dictionary (sample_id; (state, fastq files))"""
-        sample_map = {}
-        with open(self.infile) as sample_file:
-            for line in sample_file:
-                elements = line.rstrip().split("\t")
-                if len(elements) > 0 and not elements[0].startswith("#"):  # elements is an array, not a str => pylint: disable=len-as-condition
-                    try:
-                        sample_id = elements[0]
-                        state = elements[1]
-                        fastq = elements[2]
-                        sample_map[sample_id] = (state, fastq)
-                    except IndexError as idx_err:
-                        print(idx_err)
-                        print(line)
-        return sample_map
-
-    def write_file(self):
-        """Write a sample file to disk"""
-        with open(self.infile, "w") as outf:
-            outf.write("{0}\t{1}\t{2}\n".format("#Sample ID", "Performed analyses", "Corresponding fastq files"))
-            for key in self.sample_map.iterkeys():
-                outf.write("{0}\t{1}\t{2}\n".format(key, self.sample_map[key][0], self.sample_map[key][1]))
+        self.db_con = sqlite3.connect(self.infile)
+        # check if the table "samples" exists and create it if not
+        try:
+            self.db_con.execute("create table samples (sample_id varchar primary key, state varchar, fastq_file varchar)")
+        except sqlite3.OperationalError:
+            # table already exists
+            pass
 
     # getter and setter methods
+    def get_sample_id_list(self):
+        """ Return a list of sample_ids from the database """
+        db_cursor = self.db_con.cursor()
+        ids = db_cursor.execute("SELECT sample_id FROM samples").fetchall()
+        return [sid for (sid,) in ids]
+
     def get_state_string(self, sample_id):
         """Get the state of a sample based on its id"""
-        state = ""
-        if sample_id in self.sample_map:
-            state = self.sample_map[sample_id][0]
-        return state
+        db_cursor = self.db_con.cursor()
+        try:
+            return db_cursor.execute("SELECT state FROM samples WHERE sample_id='{}';".format(sample_id)).fetchone()[0]
+        except TypeError:
+            print("Sample {} is not available in the database".format(sample_id))
+            return ""
 
     def get_tool_list_from_state(self, sample_id):
         """Get a list of tools that have been successfully run on a sample based on its id"""
@@ -74,47 +61,79 @@ class Samples(object):
     def get_fastq_files(self, sample_id):
         """Get a tuple of fastq file strings"""
         fastq = ""
-        if sample_id in self.sample_map:
-            fastq = self.sample_map[sample_id][1]
+        db_cursor = self.db_con.cursor()
+        try:
+            fastq = db_cursor.execute("SELECT fastq_file FROM samples WHERE sample_id='{}';".format(sample_id)).fetchone()[0]
             if "R2" in fastq:
                 return (fastq.split(";")[0][3:], fastq.split(";")[1][3:])
             return fastq[3:]
-        return fastq
+        except TypeError:
+            print("Sample {} is not available in the database".format(sample_id))
+            return fastq
 
     def add_sample(self, sample_id, state, fastq):
         """Add a sample plus corresponding state and fastq file(s) to a sample file"""
-        if not sample_id in self.sample_map:
-            self.sample_map[sample_id] = (state, fastq)
-        self.write_file()
+        try:
+            with self.db_con:
+                self.db_con.execute("INSERT INTO samples(sample_id, state, fastq_file) values (?,?,?)", (sample_id, state, fastq))
+        except sqlite3.IntegrityError:
+            print("Sample {} is already in the table".format(sample_id))
 
     def set_state(self, sample_id, state):
         """Update, that is override, a sample plus corresponding state to a sample file"""
-        if sample_id in self.sample_map:
-            self.sample_map[sample_id] = (state, self.sample_map[sample_id][1])
-        self.write_file()
+        with self.db_con:
+            self.db_con.execute("UPDATE samples SET state = ? WHERE sample_id = ?", (state, sample_id))
 
     def append_state(self, sample_id, state):
         """Append something to an existing state or use update"""
-        if sample_id in self.sample_map:
-            if self.get_state_string(sample_id) == 'NA':
-                self.set_state(sample_id, state)
-            elif state not in self.get_state_string(sample_id):
-                self.sample_map[sample_id] = ("{0}, {1}".format(self.get_state_string(sample_id), state), self.sample_map[sample_id][1])
-        self.write_file()
+        state_str = self.get_state_string(sample_id)
+        if state_str == 'NA':
+            self.set_state(sample_id, state)
+        elif state not in state_str:
+            self.set_state(sample_id, "{0}, {1}".format(state_str, state))
+
+    def write_file(self):
+        """ Write the whole db to file """
+        db_cursor = self.db_con.cursor()
+        db_out = db_cursor.execute("SELECT * FROM samples;").fetchall()
+
+        out_file = "{}.csv".format(self.infile[:-3])
+        with open(out_file, 'w') as out:
+            out.write("#Sample ID\tPerformed analyses\tCorresponding fastq files\n")
+            for line in ["\t".join([x, y, z]) for (x, y, z) in db_out]:
+                out.write("{}\n".format(line))
+
+    def csv2db(self):
+        """ This method allows conversion from formerly used samples.csv to sqlite3 samples.sb files """
+        in_csv = "{}.csv".format(self.infile[:-3])
+        try:
+            with self.db_con:
+                with open(in_csv, "r") as in_file:
+                    for line in in_file:
+                        if not line or line.startswith("#"):
+                            continue
+                        line_splitter = line.rstrip().split("\t")
+                        self.db_con.execute("INSERT INTO samples(sample_id, state, fastq_file) values (?,?,?)", (line_splitter[0], line_splitter[1], line_splitter[2]))
+        except sqlite3.IntegrityError:
+            print("Sample {} is already in the db".format(line_splitter[0]))
 
 def main():
     """Main method, parameter handling"""
     parser = ArgumentParser(description='Handle sample db operations')
 
-    parser.add_argument('-i', '--sample-id', dest='sample_id', help='Specify the sample id to process.', required=True)
+    parser.add_argument('-i', '--sample-id', dest='sample_id', help='Specify the sample id to process.', required=False)
     parser.add_argument('-o', '--output-file', dest='output_file', help='Specify the file to save the changes into.', required=True)
-    parser.add_argument('-g', '--state', dest='state', help='Specify the new state of your sample id.', required=True)
-    parser.add_argument('-l', '--action', dest='action', choices=['append_state'], help='Select the action to do with the sample id.', required=True)
+    parser.add_argument('-g', '--state', dest='state', help='Specify the new state of your sample id.', required=False)
+    parser.add_argument('-l', '--action', dest='action', choices=['append_state', 'print_db', 'convert_db'], help='Select the action to do with the sample id.', required=True)
     args = parser.parse_args()
 
     samples = Samples(args.output_file)
     if args.action == "append_state":
         samples.append_state(args.sample_id, args.state)
+    elif args.action == "print_db":
+        samples.write_file()
+    elif args.action == "convert_db":
+        samples.csv2db()
 
 if __name__ == '__main__':
     main()
