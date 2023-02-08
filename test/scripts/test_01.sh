b44b4ff7ae4f6f5aaeecf7439d440ff9875a9d72
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2023-02-08 21:56:24 +0100

    add output folder to test

--- test/scripts/test_01.sh
+++ test/scripts/test_01.sh
@@ -8,6 +8,7 @@ echo -e "sample_name\t"`pwd`"/test/data/SRR1659960_05pc_R1.fastq.gz\t"`pwd`"/tes
 nextflow main.nf \
   -profile test,conda \
   --reference /projects/data/human/ensembl/GRCh38.86/STAR_idx/ \
+  --output $output \
   --input_files test/data/test_input.txt
 
 #test -s $output/sample_name/sample_name.mutect2.vcf || { echo "Missing output VCF file!"; exit 1; }
