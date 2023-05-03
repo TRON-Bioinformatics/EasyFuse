73f24f3409eaee06505ad1b6fe56b3f5155b203b
priesgo <pablo.riesgoferreiro@tron-mainz.de>
2023-05-04 00:02:56 +0200

    add some test assertions checking that output files do exist

--- test/scripts/test_01.sh
+++ test/scripts/test_01.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+source test/bin/assert.sh
 echo -e "sample_name\t"`pwd`"/test/data/SRR1659960_05pc_R1.fastq.gz\t"`pwd`"/test/data/SRR1659960_05pc_R2.fastq.gz" > test/data/test_input.txt
 
 nextflow main.nf \
@@ -7,3 +9,8 @@ nextflow main.nf \
   --output test/output/test1 \
   --input_files test/data/test_input.txt \
   -resume
+
+
+test -s test/output/test1/sample_name/fusions_1.csv || { echo "Missing file!"; exit 1; }
+test -s test/output/test1/sample_name/fusions.pass.csv || { echo "Missing file!"; exit 1; }
+test -s test/output/test1/sample_name/fusions.pass.all.csv || { echo "Missing file!"; exit 1; }
--- test/scripts/test_02.sh
+++ test/scripts/test_02.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+source test/bin/assert.sh
 echo -e "sample_1\t"`pwd`"/test/data/SRR1659960_05pc_R1.fastq.gz\t"`pwd`"/test/data/SRR1659960_05pc_R2.fastq.gz\n" > test/data/test_input.txt
 echo -e "sample_2\t"`pwd`"/test/data/SRR1659960_05pc_R1.fastq.gz\t"`pwd`"/test/data/SRR1659960_05pc_R2.fastq.gz" >> test/data/test_input.txt
 
@@ -8,3 +10,11 @@ nextflow main.nf \
   --output test/output/test2 \
   --input_files test/data/test_input.txt \
   -resume
+
+
+test -s test/output/test2/sample_1/fusions_1.csv || { echo "Missing file!"; exit 1; }
+test -s test/output/test2/sample_1/fusions.pass.csv || { echo "Missing file!"; exit 1; }
+test -s test/output/test2/sample_1/fusions.pass.all.csv || { echo "Missing file!"; exit 1; }
+test -s test/output/test2/sample_2/fusions_1.csv || { echo "Missing file!"; exit 1; }
+test -s test/output/test2/sample_2/fusions.pass.csv || { echo "Missing file!"; exit 1; }
+test -s test/output/test2/sample_2/fusions.pass.all.csv || { echo "Missing file!"; exit 1; }
