bb7a9b46e18feec071af2cd75bf2ae704bd446a9
Patrick Sorn <patrick.sorn@tron-mainz.de>
2024-02-07 16:21:38 +0100

    Added unit and integration tests

---
+++ tests/filtering/test_fusionreadfilter.py
@@ -0,0 +1,4 @@
+from bin.fusionreadfilter import FusionReadFilter
+
+def test_fusionreadfilter():
+    pass
---
+++ tests/merging/test_merge_data.py
@@ -0,0 +1,4 @@
+#from bin.merge_data import FusionSummary
+
+def test_normalize_counts_cpm():
+    pass
